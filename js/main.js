b82ca9fb2b46135b23b544e90a475a91c0b8a12a
PelykhEvgeniy <jeka07094@gmail.com>
2023-02-19 15:12:19 +0300

    add project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const burgerBtn = document.querySelector(".burger");
+
+burgerBtn.addEventListener("click", ()=>{
+    console.log('====================================');
+    console.log(1);
+    console.log('====================================');
+})
